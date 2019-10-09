d7f1f929d16d5de5e735395c2170bc5c6d8bce88
Ashrf Ali <ashrftvm1@gmail.com>
2019-10-09 15:11:25 +0400

    api call

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 const electron = require("electron");
 const path = require("path");
 const BrowserWindow = electron.remote.BrowserWindow;
+const axios = require("axios");
 
 const notifyBtn = document.getElementById("notifyBtn");
+const price = document.querySelector("h1");
+const targetPrice = document.getElementById("targetPrice");
+
+function getBTC() {
+  axios
+    .get(
+      "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC&tsyms=USD"
+    )
+    .then(res => {
+      const cryptos = res.data.BTC.USD;
+      price.innerHTML = "$" + cryptos.toLocaleString("en");
+    });
+}
+
+getBTC();
+setInterval(getBTC, 30000);
+
 notifyBtn.addEventListener("click", e => {
   const modalPath = path.join("file://", __dirname, "add.html");
   let win = new BrowserWindow({
@@ -20,7 +38,7 @@ notifyBtn.addEventListener("click", e => {
     win = null;
   });
   // Open the DevTools.
-  win.webContents.openDevTools();
+  //   win.webContents.openDevTools();
   win.loadURL(modalPath);
   win.show();
 });
