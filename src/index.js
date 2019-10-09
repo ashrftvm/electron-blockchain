ee73c934d1854d245428ed03a40525bae2537dd1
Ashrf Ali <ashrftvm1@gmail.com>
2019-10-09 15:46:24 +0400

    completed notification

--- src/index.js
+++ src/index.js
@@ -7,6 +7,13 @@ const ipc = require("electron").ipcRenderer;
 const notifyBtn = document.getElementById("notifyBtn");
 const price = document.querySelector("h1");
 const targetPrice = document.getElementById("targetPrice");
+let targetPriceVal;
+
+const notification = {
+  title: "BTC Alert",
+  body: "BTC just beat your target price",
+  icon: path.join(__dirname, "../assets/images/btc.ico")
+};
 
 function getBTC() {
   axios
@@ -16,11 +23,17 @@ function getBTC() {
     .then(res => {
       const cryptos = res.data.BTC.USD;
       price.innerHTML = "$" + cryptos.toLocaleString("en");
+      if (targetPrice.innerHTML !== "" && targetPriceVal < res.data.BTC.USD) {
+        const myNotification = new window.Notification(
+          notification.title,
+          notification
+        );
+      }
     });
 }
 
 getBTC();
-setInterval(getBTC, 30000);
+setInterval(getBTC, 10000);
 
 notifyBtn.addEventListener("click", e => {
   const modalPath = path.join("file://", __dirname, "add.html");
